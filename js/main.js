7b74e6af2a4ac12c310cb78658e42b854bba44eb
KierstonHill83 <khill1218@gmail.com>
2015-09-16 13:09:18 -0600

    Fixed the ping pong score keeper

--- js/main.js
+++ js/main.js
@@ -76,6 +76,58 @@ app.controller('MadLibs', ['$scope', function($scope) {
   $scope.verb = 'hops'
 }]);
 
+app.controller('pingPong', ['$scope', function($scope) {
+
+    $scope.count = 0;
+    $scope.count2 = 0;
+
+  $scope.reset = function() {
+    $scope.count = 0;
+    $scope.count2 = 0;
+    $scope.message1 = "";
+    $scope.message2 = "";
+  };
+
+  $scope.serve = function() {
+    if ($scope.count + $scope.count2 < 2) {
+      $scope.message1 = "Serving";
+      $scope.message2 = "";
+    } else if ($scope.count + $scope.count2 < 4) {
+      $scope.message2 = "Serving";
+      $scope.message1 = "";
+    } else if ($scope.count + $scope.count2 < 6) {
+      $scope.message1 = "Serving";
+      $scope.message2 = "";
+    } else if ($scope.count + $scope.count2 < 8) {
+      $scope.message2 = "Serving";
+      $scope.message1 = "";
+    } else if ($scope.count + $scope.count2 < 10) {
+      $scope.message1 = "Serving";
+      $scope.message2 = "";
+    } else if ($scope.count + $scope.count2 < 12) {
+      $scope.message2 = "Serving";
+      $scope.message1 = "";
+    } else if ($scope.count + $scope.count2 < 14) {
+      $scope.message1 = "Serving";
+      $scope.message2 = "";
+    } else if ($scope.count + $scope.count2 < 16) {
+      $scope.message2 = "Serving";
+      $scope.message1 = "";
+    } else if ($scope.count + $scope.count2 < 18) {
+      $scope.message1 = "Serving";
+      $scope.message2 = "";
+    } else if ($scope.count + $scope.count2 < 20) {
+      $scope.message2 = "Serving";
+      $scope.message1 = "";
+    } else if ($scope.count + $scope.count2 < 22) {
+      $scope.message1 = "Serving";
+      $scope.message2 = "";
+    }
+  };
+
+}]);
+
+
 app.controller("reverseWords", function($scope) {
   $scope.reverseTheWords = function(word) {
     $scope.reverse = word.split("").reverse().join("");
